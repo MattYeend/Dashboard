e301e1f04c00f287a4fd0c3b078eb8545f5a3f57
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-05-24 13:06:41 +0100

    #8 Updated fortify

--- config/fortify.php
+++ config/fortify.php
@@ -164,6 +164,8 @@ return [
         Features::registration(),
         Features::resetPasswords(),
         Features::emailVerification(),
+        Features::updateProfileInformation(),
+        Features::updatePasswords(),
         Features::twoFactorAuthentication([
             'confirm' => true,
             'confirmPassword' => true,
