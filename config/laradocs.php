d9d6a8ab72163bc3688f42d7b52e1e5e33ecab25
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-06-25 12:00:37 +0100

    #88 Update to laradocs config and .env.example

--- config/laradocs.php
+++ config/laradocs.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'enabled' => env('LARADOCS_ENABLED', true),
+    'enabled' => env('LARADOCS_ENABLED', ! app()->isProduction()),
 
     /*
     |--------------------------------------------------------------------------
@@ -34,7 +34,7 @@ return [
     */
 
     'route' => [
-        'register' => env('LARADOCS_ROUTE_REGISTER', true),
+        'register' => env('LARADOCS_ROUTE_REGISTER', ! app()->isProduction()),
         'prefix' => env('LARADOCS_ROUTE_PREFIX', 'docs'),
         'domain' => env('LARADOCS_ROUTE_DOMAIN'),
         'middleware' => ['web'],
