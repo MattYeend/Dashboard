b382b5e34b82f7a056612aa5f5547b648ab58a7c
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-07-10 09:12:39 +0100

    #142 New migration to add plan id to subscriptions

---
+++ database/migrations/2026_07_10_081152_add_plan_id_to_subscriptions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->foreignId('plan_id')
+                ->nullable()
+                ->after('user_id')
+                ->constrained('plans')
+                ->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('plan_id');
+        });
+    }
+};
