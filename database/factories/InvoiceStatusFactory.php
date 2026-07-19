2eeab6a82dfc70cda2f835ccd32b8f9aad643783
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-07-19 10:04:00 +0100

    #178 Update to invoice status factory to add deleted function

--- database/factories/InvoiceStatusFactory.php
+++ database/factories/InvoiceStatusFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\InvoiceStatus;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class InvoiceStatusFactory extends Factory
@@ -18,5 +19,16 @@ class InvoiceStatusFactory extends Factory
             'text_colour' => $this->faker->hexColor(),
             'meta' => null,
         ];
+    }
+
+    /**
+     * Indicate that the invoice status is soft-deleted.
+     */
+    public function deleted(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'deleted_at' => now(),
+            'deleted_by' => User::factory(),
+        ]);
     }
 }
