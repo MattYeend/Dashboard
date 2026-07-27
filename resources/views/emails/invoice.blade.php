641869cbae1f494f13d1b42ab74ea8230efaef77
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-07-27 09:14:04 +0100

    #192 Changes to invoice email view

--- resources/views/emails/invoice.blade.php
+++ resources/views/emails/invoice.blade.php
@@ -1,5 +1,16 @@
-<p>Hello,</p>
+<x-mail::message>
+# Invoice {{ $invoice->invoice_number ?? '#' . $invoice->id }}
 
-<p>Please find attached invoice {{ $invoice->invoice_number ?? '#' . $invoice->id }}.</p>
+Hello,
 
-<p>Kind regards</p>
+Please find attached your invoice from {{ config('app.name') }}.
+
+<x-mail::button :url="route('invoices.pdf', $invoice)">
+View Invoice
+</x-mail::button>
+
+If you have any questions about this invoice, please get in touch.
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>
