cbcbf23932ea0c54eee23e066040b4911672d2de
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-07-31 12:04:10 +0100

    #241 Finised off removing example tests

--- tests/Feature/OrderTest.php
+++ tests/Feature/OrderTest.php
@@ -21,11 +21,6 @@ beforeEach(function () {
     Role::firstOrCreate(['name' => 'User']);
 });
 
-test('example', function () {
-    $response = $this->get('/');
-    $response->assertStatus(200);
-});
-
 describe('index', function () {
     test('authenticated user with permission can list orders', function () {
         $superAdmin = $this->superAdminUser();
