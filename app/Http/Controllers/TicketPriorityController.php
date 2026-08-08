f1b463bac6bea62fba7539c64e9b56dca3a89945
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-08-08 09:03:26 +0100

    #277 New cli -a commands for tickets, priority and status

---
+++ app/Http/Controllers/TicketPriorityController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTicketPriorityRequest;
+use App\Http\Requests\UpdateTicketPriorityRequest;
+use App\Models\TicketPriority;
+
+class TicketPriorityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTicketPriorityRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TicketPriority $ticketPriority)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TicketPriority $ticketPriority)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTicketPriorityRequest $request, TicketPriority $ticketPriority)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TicketPriority $ticketPriority)
+    {
+        //
+    }
+}
