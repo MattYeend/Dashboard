9aeeb355df39c5a77903ac9aeb1da928fa6d8f16
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-08-14 10:37:35 +0100

    #299 Update to auth controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -62,6 +62,8 @@ class AuthController extends Controller
         }
 
         Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
 
         return response()->json(['message' => 'Logged out']);
     }
