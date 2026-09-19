690009c485e015d0b73f649f5a29403a7dc5d265
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-09-19 07:22:58 +0100

    #431 Add box border and heading to PermissionRoleDetails.vue

--- resources/js/pages/Permissions/components/PermissionRoleDetails.vue
+++ resources/js/pages/Permissions/components/PermissionRoleDetails.vue
@@ -5,15 +5,19 @@ defineProps<{ permission: Permission }>();
 </script>
 
 <template>
-    <dl>
-        <dt class="text-xs text-gray-400">Assigned roles</dt>
-        <dd class="text-sm">
-            <span v-if="!permission.roles?.length" class="text-gray-400">
-                None
-            </span>
-            <span v-else>
-                {{ permission.roles.map((role) => role.name).join(', ') }}
-            </span>
-        </dd>
-    </dl>
+    <div class="rounded-lg border p-4">
+        <h2 class="mb-4 text-sm font-medium text-gray-400">Role details</h2>
+
+        <dl>
+            <dt class="text-xs text-gray-400">Assigned roles</dt>
+            <dd class="text-sm">
+                <span v-if="!permission.roles?.length" class="text-gray-400">
+                    None
+                </span>
+                <span v-else>
+                    {{ permission.roles.map((role) => role.name).join(', ') }}
+                </span>
+            </dd>
+        </dl>
+    </div>
 </template>
