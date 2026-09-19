1738fa57571059f680f80f22617c0f127d1858b3
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-09-19 07:22:26 +0100

    #431 Add box border and heading to PermissionBasicDetails.vue

--- resources/js/pages/Permissions/components/PermissionBasicDetails.vue
+++ resources/js/pages/Permissions/components/PermissionBasicDetails.vue
@@ -5,14 +5,18 @@ defineProps<{ permission: Permission }>();
 </script>
 
 <template>
-    <dl class="grid grid-cols-2 gap-4">
-        <div>
-            <dt class="text-xs text-gray-400">Name</dt>
-            <dd class="text-sm">{{ permission.name }}</dd>
-        </div>
-        <div>
-            <dt class="text-xs text-gray-400">Guard</dt>
-            <dd class="text-sm">{{ permission.guard_name }}</dd>
-        </div>
-    </dl>
+    <div class="rounded-lg border p-4">
+        <h2 class="mb-4 text-sm font-medium text-gray-400">Basic details</h2>
+
+        <dl class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+            <div>
+                <dt class="text-xs text-gray-400">Name</dt>
+                <dd class="text-sm">{{ permission.name }}</dd>
+            </div>
+            <div>
+                <dt class="text-xs text-gray-400">Guard</dt>
+                <dd class="text-sm">{{ permission.guard_name }}</dd>
+            </div>
+        </dl>
+    </div>
 </template>
