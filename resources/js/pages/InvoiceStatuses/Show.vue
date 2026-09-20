9ceaededb751d7fc6b38a277ce5b9367462be1aa
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-09-20 09:33:11 +0100

    #432 Update to styling on invoice statuses show page

--- resources/js/pages/InvoiceStatuses/Show.vue
+++ resources/js/pages/InvoiceStatuses/Show.vue
@@ -48,12 +48,14 @@ function destroy(): void {
 
 <template>
     <div class="py-6">
-        <div class="mx-auto max-w-3xl px-4 sm:px-6 lg:px-8">
-            <div class="mb-6 flex items-center justify-between">
+        <div class="mx-auto px-4 sm:px-6 lg:px-8">
+            <div
+                class="mb-6 flex flex-wrap items-center justify-between gap-4 border-b pb-4"
+            >
                 <h1 class="text-2xl font-semibold text-gray-300">
                     {{ invoiceStatus.title }}
                 </h1>
-                <div class="space-x-2">
+                <div class="ml-auto flex flex-wrap gap-2">
                     <Link
                         :href="invoiceStatusesIndex.url()"
                         class="inline-flex items-center rounded-md px-4 py-2 text-sm font-medium"
